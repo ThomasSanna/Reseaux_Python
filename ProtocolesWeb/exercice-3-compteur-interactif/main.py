f29cc0f59ca85fcb7849791aa8c57023a351300f
WadeeKT <thomassanna1e@gmail.com>
2024-02-21 14:12:14 +0100

    Init fastapi, compteur interactif avec serveur, fastapi + unicorn + fetch

--- ProtocolesWeb/exercice-3-compteur-interactif/main.py
+++ ProtocolesWeb/exercice-3-compteur-interactif/main.py
@@ -1,27 +1,36 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI() # Création de l'application FastAPI
 
 app.mount("/static", StaticFiles(directory="static"), name="static") # Montage du dossier static =
 
-counter = 0 # Initialisation du compteur
+counter = 0 # initialisation du compteur
 
-@app.get("/") # Définition de la route
-async def read_root(): # Définition de la fonction associée à la route  
-    return FileResponse('static/index.html') # Retourne le fichier index.html
+@app.get("/") # définition de la route
+def read_root(): # définition de la fonction associée à la route  
+    return FileResponse('static/index.html') # retourne le fichier index.html
 
 @app.get("/increment")
-async def increment_counter():
+def increment_counter():
     global counter
-    counter += 1 # Incrémentation de la variable counter
-    return {"counter": counter} # Retourne la valeur de counter
+    counter += 1 # incrémentation de la variable counter
+    return {"counter": counter} # retourne la valeur de counter
 
 @app.get("/decrement") 
-async def decrement_counter():
+# async def....
+def decrement_counter():
     global counter
     counter -= 1
     return {"counter": counter}
 
+@app.get("/actualisation")
+def actualisation():
+    return {"counter": counter}
+
 # uvicorn main:app --reload
+
+if __name__ == '__main__':
+    uvicorn.run('main:app')
